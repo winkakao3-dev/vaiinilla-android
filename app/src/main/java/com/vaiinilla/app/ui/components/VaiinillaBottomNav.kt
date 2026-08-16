b63c6c27eba252d9b89d10daafd28564d72e824b
kakao <kakao@kakaos-MacBook-Air.local>
2026-08-16 14:42:52 -0600

    Fix bottom navigation state delegation

--- app/src/main/java/com/vaiinilla/app/ui/components/VaiinillaBottomNav.kt
+++ app/src/main/java/com/vaiinilla/app/ui/components/VaiinillaBottomNav.kt
@@ -46,6 +46,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
