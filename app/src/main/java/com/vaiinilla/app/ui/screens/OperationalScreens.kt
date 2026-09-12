7bd3fde06dd0c694eec87915de26ad21f6c65397
root <root@vmi3532036.contaboserver.net>
2026-09-12 15:26:57 -0600

    fix(ops): import defaultMinSize faltante

--- app/src/main/java/com/vaiinilla/app/ui/screens/OperationalScreens.kt
+++ app/src/main/java/com/vaiinilla/app/ui/screens/OperationalScreens.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
